06661b3b2425514594aabbc8c0e0a6797f984eb8
VanMinh15 <minhcangthang@gmail.com>
2024-03-22 02:56:47 +0700

    add basic design

---
+++ Angular/tutorial/src/app/components/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Product } from '../../../type';
+import { RatingModule } from 'primeng/rating';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RatingModule, FormsModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  ngOnInit() {
+    this.productOutput.emit(this.product);
+  }
+
+}
